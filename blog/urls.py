aabc228b607a9c2c3c8299dcd41aae243bc7f45d
nyars15 <nyars15@gmail.com>
2021-07-06 02:28:00 +0000

    commit after signup page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
+from django.conf.urls import url
 from django.urls import path
 from .import views
 
+
+
 urlpatterns = [
-    path('', views.post_list, name="post_list"),
+    url(r'^signup/$',      views.signup, name="signup"),
+    path('',               views.post_list, name="post_list"),
     path('post/<int:pk>/', views.post_detail, name="post_detail"),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
+from .forms import SignUpForm
 from .models import Post
 
 # Create your views here.
 # def post_list(request):
 #     return(render(request, "blog/post_list.html", {}))
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect("home")
+    else:
+        form = SignUpForm()
+    return render(request, "blog/signup.html", {"form": form})
+
+
 
 def post_list(request):
-    posts= Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, "blog/post_list.html", {"posts": posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
